f453386eb48cf6b7ca457822b6da9b55d16ee43f
Muhammed-Shahid <shahid04m@gmail.com>
2023-10-26 19:22:32 +0530

    first commit

--- loomix_rentals/settings.py
+++ loomix_rentals/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-^_z(g(vo!@+rvpfq@2#_0rnq9umlkm#=ujjn+zqgqa_sw4hsn3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.232.26.226']
+ALLOWED_HOSTS = ['13.232.26.226','127.0.0.1','localhost']
 
 
 # Application definition
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Add your front-end domain here
+    "http://localhost:3000",
+      'http://13.232.26.226:80'  # Add your front-end domain here
 ]
 
 
