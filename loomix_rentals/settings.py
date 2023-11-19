d361b14e7b267ed35b098738fd9793125f95e613
Muhammed-Shahid <shahid04m@gmail.com>
2023-11-19 17:44:43 +0530

    revenue chart

--- loomix_rentals/settings.py
+++ loomix_rentals/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "accounts.middlewares.block_middleware.MyMiddleware"
+    "accounts.Middlewares.block_middleware.MyMiddleware"
     
 ]
 
